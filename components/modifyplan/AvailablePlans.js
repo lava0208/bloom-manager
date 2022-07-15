abc17b3cec5bd380869fe36388764135a315eabb
Michael Podolsky <michaelspodolsky@gmail.com>
2022-07-14 21:45:59 -0700

    add available plans component

---
+++ components/modifyplan/AvailablePlans.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { availablePlans } from "~lib/dummy";
+
+import styles from "~styles/components/modifyplan/availableplans.module.scss";
+
+const AvailablePlans = () => {
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <h2>Available</h2>
+        <button className={styles.searchButton}>Search</button>
+      </div>
+      <div className={styles.plansContainer}>
+        {availablePlans.map((plan) => (
+          <div className={styles.planContainer}>
+            <div className={styles.planImage}></div>
+            <div className={styles.planInfoContainer}>
+              <h3>{plan.name}</h3>
+              <h4>{plan.variety}</h4>
+              <h5>{plan.description}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AvailablePlans;
--- pages/modifyplan.js
+++ pages/modifyplan.js
@@ -4,6 +4,7 @@ import styles from "~styles/pages/modifyplan.module.scss";
 
 import Sidebar from "~components/Sidebar";
 import CurrentPlan from "~components/modifyplan/CurrentPlan";
+import AvailablePlans from "~components/modifyplan/AvailablePlans";
 import YourPlan from "~components/modifyplan/YourPlan";
 
 const ModifyPlan = () => {
@@ -14,6 +15,7 @@ const ModifyPlan = () => {
         <h1 className={styles.header}>2022 Season</h1>
         <h2 className={styles.subHeader}>Modify Plan</h2>
         <CurrentPlan />
+        <AvailablePlans />
       </div>
       <YourPlan />
     </div>
