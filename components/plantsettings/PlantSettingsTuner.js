61a5c2271b08ad25daaff4025af4238bb9df0d3d
Michael Podolsky <michaelspodolsky@gmail.com>
2022-07-14 22:36:58 -0700

    create plant settings component

--- components/plantsettings/PlantSettingsTuner.js
+++ components/plantsettings/PlantSettingsTuner.js
@@ -12,6 +12,91 @@ const PlantSettingsTuner = () => {
 
   return (
     <div className={styles.container}>
+      {/* Plant Settings */}
+      <div className={styles.plantSettingsContainer}>
+        <div className={styles.plantSettingsPaper}>
+          <div className={styles.plantSettingsColumn}>
+            <div className={styles.plantSettingsRow}>
+              <h3>Name</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Species</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Earliest Seed</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Latest Seed</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Harden</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Transplant</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Maturity</h3>
+              <input type="text" />
+            </div>
+          </div>
+          <div className={styles.plantSettingsColumn}>
+            <div className={styles.plantSettingsRow}>
+              <h3>Light</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Depth</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Direct Sow</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Seed Note</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Transplant Note</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Harvest Note</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Pinch Note</h3>
+              <input type="text" />
+            </div>
+          </div>
+          <div className={styles.plantSettingsColumn}>
+            <div className={styles.plantSettingsRow}>
+              <h3>Pinch</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Harvest Length</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsRow}>
+              <h3>Potting On</h3>
+              <input type="text" />
+            </div>
+            <div className={styles.plantSettingsImage}></div>
+          </div>
+        </div>
+        <div className={styles.settingsButtonContainer}>
+          <button className={styles.settingsButton}>Save</button>
+          <button className={styles.settingsButton}>Reset</button>
+        </div>
+      </div>
+
       {/* User Settings */}
       <div className={styles.userSettingsContainer}>
         <div className={styles.userSettingsPaper}>
