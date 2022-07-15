4e13ce83b71b90b4acd0442f107baf5536bbf6c4
Michael Podolsky <michaelspodolsky@gmail.com>
2022-07-14 22:46:52 -0700

    hook up state to plant settings

--- components/plantsettings/PlantSettingsTuner.js
+++ components/plantsettings/PlantSettingsTuner.js
@@ -3,6 +3,26 @@ import React, { useState } from "react";
 import styles from "~styles/components/plantsettings/plantsettingstuner.module.scss";
 
 const PlantSettingsTuner = () => {
+  const [plantSettings, setPlantSettings] = useState({
+    name: "",
+    species: "",
+    earliestSeed: "",
+    latestSeed: "",
+    harden: "",
+    transplant: "",
+    maturity: "",
+    light: "",
+    depth: "",
+    directSow: "",
+    seedNote: "",
+    transplantNote: "",
+    harvestNote: "",
+    pinchNote: "",
+    pinch: "",
+    harvestLength: "",
+    pottingOn: "",
+  });
+
   const [userSettings, setUserSettings] = useState({
     lastFrost: "",
     firstFrost: "",
@@ -18,75 +38,216 @@ const PlantSettingsTuner = () => {
           <div className={styles.plantSettingsColumn}>
             <div className={styles.plantSettingsRow}>
               <h3>Name</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.name}
+                onChange={(e) =>
+                  setPlantSettings({ ...plantSettings, name: e.target.value })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Species</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.species}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    species: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Earliest Seed</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.earliestSeed}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    earliestSeed: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Latest Seed</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.latestSeed}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    latestSeed: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Harden</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.harden}
+                onChange={(e) =>
+                  setPlantSettings({ ...plantSettings, harden: e.target.value })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Transplant</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.transplant}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    transplant: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Maturity</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.maturity}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    maturity: e.target.value,
+                  })
+                }
+              />
             </div>
           </div>
           <div className={styles.plantSettingsColumn}>
             <div className={styles.plantSettingsRow}>
               <h3>Light</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.light}
+                onChange={(e) =>
+                  setPlantSettings({ ...plantSettings, light: e.target.value })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Depth</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.depth}
+                onChange={(e) =>
+                  setPlantSettings({ ...plantSettings, depth: e.target.value })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Direct Sow</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.directSow}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    directSow: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Seed Note</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.seedNote}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    seedNote: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Transplant Note</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.transplantNote}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    transplantNote: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Harvest Note</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.harvestNote}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    harvestNote: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Pinch Note</h3>
-              <input type="text" />
+              <input
+                type="text"
+                placeholder="Optional"
+                value={plantSettings.pinchNote}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    pinchNote: e.target.value,
+                  })
+                }
+              />
             </div>
           </div>
           <div className={styles.plantSettingsColumn}>
             <div className={styles.plantSettingsRow}>
               <h3>Pinch</h3>
-              <input type="text" />
+              <input
+                type="text"
+                placeholder="Optional"
+                value={plantSettings.pinch}
+                onChange={(e) =>
+                  setPlantSettings({ ...plantSettings, pinch: e.target.value })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Harvest Length</h3>
-              <input type="text" />
+              <input
+                type="text"
+                value={plantSettings.harvestLength}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    harvestLength: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsRow}>
               <h3>Potting On</h3>
-              <input type="text" />
+              <input
+                type="text"
+                placeholder="Optional"
+                value={plantSettings.pottingOn}
+                onChange={(e) =>
+                  setPlantSettings({
+                    ...plantSettings,
+                    pottingOn: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className={styles.plantSettingsImage}></div>
           </div>
