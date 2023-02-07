120b1ae6f79d0634fd874995fe469151f06782b4
lava_0208 <lava_0208@hotmail.com>
2023-02-07 03:35:48 -0500

    create a plan api

--- pages/api/plans.js
+++ pages/api/plans.js
@@ -6,10 +6,16 @@ export default async function handler(req, res) {
     const db = client.db("bloom");
 
     switch (req.method) {
+        //... create a plan
+        case "POST":
+            await db.collection("plans").insertOne(req.body);
+            return res.json({ status: true, data: 'A plan is created successfully.' });
+
         //... get all plans or plan by id
         case "GET":
             const { planid } = req.query;
-            if(planid === null){
+            console.log(planid);
+            if(planid === undefined){
                 let plans = await db.collection("plans").find({}).toArray();
                 return res.json({ status: true, data: plans });
             }else{
@@ -30,7 +36,7 @@ export default async function handler(req, res) {
                         seeding: req.body.seeding,  //... direct sow = 1, start indoors = 2
                         quantity: req.body.quantity,
                         harvest: req.body.harvest,  //... early = 1, regular = 2, late = 3
-                        plantings: req.body.harvest,
+                        plantings: req.body.plantings,
                         days: req.body.days,
                         option: req.body.option //... start = 1, regular = 2, pinch = 3, pot on = 4
                     },
