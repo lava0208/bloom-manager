c8a4a5e806c39def85a761dd0d56b78a1807c780
lava_0208 <lava_0208@hotmail.com>
2023-02-08 16:31:38 -0500

    update planting api

---
+++ pages/api/plantings.js
@@ -0,0 +1,44 @@
+import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+    const client = await clientPromise;
+    const db = client.db("bloom");
+
+    switch (req.method) {
+        //... create plantings
+        case "POST":
+            await db.collection("plantings").insertOne(req.body);
+            return res.json({ status: true, data: 'Planting is created successfully.' });
+
+        //... get all plantings or planing by id
+        case "GET":
+            const { plantingid } = req.query;
+            if(plantingid === undefined){
+                let plantings = await db.collection("plantings").find({}).toArray();
+                return res.json({ status: true, data: plantings });
+            }else{
+                let plan = await db.collection("plantings").findOne({_id: new ObjectId(plantingid)});
+                return res.json({ status: true, data: plan });
+            }
+
+        //... update a plan
+        case "PUT":
+            const { id } = req.query;
+            await db.collection("plantings").updateOne(
+                {
+                    _id: new ObjectId(id),
+                },
+                {
+                    $set: {
+                        seeds: req.body.seeds,
+                        harvest: req.body.harvest,
+                        direct_sow: req.body.direct_sow,
+                        pinch: req.body.pinch,
+                        pot_on: req.body.pot_on
+                    },
+                }
+            );
+            return res.json({ status: true, message: 'Planting is updated successfully.' });
+    }
+}
