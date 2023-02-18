9334604ad784935627d3aeaf4834acd9477df7eb
jorge <jorgencnc0608@outlook.com>
2023-02-17 19:52:39 -0500

    list api integration

--- components/masterplan/List.js
+++ components/masterplan/List.js
@@ -1,9 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { Modal, ModalBody } from "reactstrap";
 
-import { list } from "~lib/dummy";
+import { taskService } from "services";
+
+import { dummyList } from "~lib/dummy";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from "~styles/components/masterplan/list.module.scss";
@@ -26,6 +28,23 @@ const List = () => {
         setEvent(event)
     }
 
+    const [todayTasks, setTodayTasks] = useState([]);
+    const [tomorrowTasks, setTomorrowTasks] = useState([]);
+    const [weekTasks, setWeekTasks] = useState([]);
+    const [overdueTasks, setOverdueTasks] = useState([]);
+
+    useEffect(() => {
+        getAllTasks();
+    }, [])
+
+    const getAllTasks = async () => {
+        var _result = await taskService.getAllByDate();
+        setTodayTasks(_result.data.today);
+        setTomorrowTasks(_result.data.tomorrow);
+        setWeekTasks(_result.data.week);
+        setOverdueTasks(_result.data.overdue);
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.tasksContainer}>
@@ -33,12 +52,12 @@ const List = () => {
                     Overdue
                 </h2>
                 <div className={styles.tasksScrollContainer}>
-                    {list.overdue.map((task, i) => (
+                    {overdueTasks.map((task, i) => (
                         <div className={styles.taskContainer} key={i}>
                             <div className={styles.taskInfo}>
-                                <h2>{task.name}</h2>
+                                <h2>{task.title}</h2>
                                 <h3 className={styles.overdue}>
-                                    {moment(task.date).fromNow()}
+                                    {moment(task.scheduled_at).fromNow()}
                                 </h3>
                             </div>
                             <div className={`${styles.taskCap} ${styles.overdue}`}></div>
@@ -49,7 +68,7 @@ const List = () => {
             <div className={styles.tasksContainer}>
                 <h2 className={`${styles.tasksContainerTitle} `}>Today</h2>
                 <div className={styles.tasksScrollContainer}>
-                    {list.today.map((task, i) => (
+                    {dummyList.today.map((task, i) => (
                         <div className={styles.taskContainer} key={i} onClick={() => openSchedule(task)}>
                             <div className={styles.taskInfo}>
                                 <h2>{task.title}</h2>
@@ -69,10 +88,10 @@ const List = () => {
             <div className={styles.tasksContainer}>
                 <h2 className={`${styles.tasksContainerTitle} `}>Tomorrow</h2>
                 <div className={styles.tasksScrollContainer}>
-                    {list.tomorrow.map((task, i) => (
+                    {tomorrowTasks.map((task, i) => (
                         <div className={styles.taskContainer} key={i}>
                             <div className={styles.taskInfo}>
-                                <h2>{task.name}</h2>
+                                <h2>{task.title}</h2>
                                 <h3>Tomorrow</h3>
                             </div>
                             <div className={`${styles.taskCap} ${styles.tomorrow}`}></div>
@@ -83,9 +102,9 @@ const List = () => {
             <div className={styles.thisWeekContainer}>
                 <h2>This Week</h2>
                 <div className={styles.thisWeekScrollContainer}>
-                    {list.thisWeek.map((task, i) => (
+                    {weekTasks.map((task, i) => (
                         <div className={styles.thisWeekTaskContainer} key={i}>
-                            <h3>{task.name}</h3>
+                            <h3>{task.title}</h3>
                             <h4>{moment(task.date).format("dddd t\\h\\e Do")}</h4>
                         </div>
                     ))}
